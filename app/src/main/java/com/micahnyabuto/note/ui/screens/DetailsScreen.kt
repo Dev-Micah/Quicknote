a8b252a9e3897dd4db7ffd3887fc0571e281ec2e
Micah Nyabuto <atembamicah@gmail.com>
2025-02-23 23:02:17 +0300

    added detailscreen

--- app/src/main/java/com/micahnyabuto/note/ui/screens/DetailsScreen.kt
+++ app/src/main/java/com/micahnyabuto/note/ui/screens/DetailsScreen.kt
@@ -1,20 +1,186 @@
 package com.micahnyabuto.note.ui.screens
 
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.micahnyabuto.note.data.Note
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NoteDetailScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Note details") },
+//                canNavigateBack = true,
+//                navigateUp = navigateBack
+                navigationIcon = {
+                    IconButton(onClick = { navigateBack() }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "NavigateUp"
+                        )
+                    }
+                },
+
+            )
+
+        }, floatingActionButton = {
+            FloatingActionButton(
+                onClick = {  },
+                shape = RoundedCornerShape(70.dp),
+                modifier = Modifier.padding(
+                    bottom = 16.dp,
+                    end = 16.dp
+                )
+
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "Edit",
+                )
+            }
+        }, modifier = modifier
+    ) { innerPadding ->
+        NoteDetailsBody(
+            //itemDetailsUiState = ItemDetailsUiState(),
+            onClick = {},
+            onDelete = { },
+            modifier = Modifier
+                .padding(
+                    start = innerPadding.calculateStartPadding(LocalLayoutDirection.current),
+                    end = innerPadding.calculateEndPadding(LocalLayoutDirection.current),
+                    top = innerPadding.calculateTopPadding()
+                )
+                .verticalScroll(rememberScrollState())
+        )
+    }
+}
+@Composable
+private fun NoteDetailsBody(
+//    itemDetailsUiState: ItemDetailsUiState,
+    onDelete: () -> Unit,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.padding(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+
+        NoteDetails(
+            //item = itemDetailsUiState.itemDetails.toItem(),
+            note = toString(),
+            modifier = Modifier.fillMaxWidth()
+        )
+        IconButton(onClick = { deleteConfirmationRequired = true },
+            modifier = Modifier.padding(
+                top = 16.dp,
+                end = 16.dp
+            )){
+
+            Icon(
+                imageVector = Icons.Default.Delete,
+                contentDescription = "Delete"
+            )
+        }
+
+
+
+        if (deleteConfirmationRequired) {
+            DeleteConfirmationDialog(
+                onDeleteConfirm = {
+                    deleteConfirmationRequired = false
+                    onDelete()
+                },
+                onDeleteCancel = { deleteConfirmationRequired = false },
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+    }
+    }
+@Composable
+fun NoteDetails(
+    note: String,
+    modifier: Modifier = Modifier
+){
+    Card(
+        modifier = modifier,
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ){
+}
+    Column(
+        modifier = Modifier.padding(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        NoteDetailsRow(
+            //labelResID = note.id,
+            text = note,
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth()
+        )
+}
+    }
 
 @Composable
-fun DetailScreen(){
+fun NoteDetailsRow(
+    //@StringRes labelResID: Int, noteDetails: String,
+    modifier: Modifier = Modifier,
+    text: String
+){
 
 }
 
 
+
+
 @Composable
-private fun DeleteConfirmationDialog(
+fun DeleteConfirmationDialog(
     onDeleteConfirm: () -> Unit,
     onDeleteCancel: () -> Unit,
     modifier: Modifier = Modifier
