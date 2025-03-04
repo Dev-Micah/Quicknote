d04429c74992c9eafd3935522128a19d5f5bec6e
Micah Nyabuto <atembamicah@gmail.com>
2025-03-04 23:11:35 +0300

    Update

--- app/src/main/java/com/micahnyabuto/note/data/AppContainer.kt
+++ app/src/main/java/com/micahnyabuto/note/data/AppContainer.kt
@@ -1,2 +1,17 @@
 package com.micahnyabuto.note.data
 
+import android.content.Context
+
+interface AppContainer{
+    val notesRepository: NotesRepository
+}
+
+class AppDataContainer(private val context: Context): AppContainer{
+
+    override val notesRepository: NotesRepository by lazy {
+        OfflineNotesRepository()
+    }
+
+
+
+}
